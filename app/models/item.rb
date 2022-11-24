5682dedc2ea793bac18313c5b39faf51138ef521
ponta4699 <ponta4699@gmail.com>
2022-11-24 21:41:56 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,16 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+ 
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :prefecture
+  belongs_to :shipping_fee_status
+  belongs_to :scheduled_delivery
+
   has_one_attached :image
   belongs_to :user
-  
+
+
 end
