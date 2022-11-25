6c9587873ac2e7b5c419e4ce6f9d993a2a83abe5
ponta4699 <ponta4699@gmail.com>
2022-11-25 13:50:51 +0900

    手順6 出品した商品の情報をテーブルに保存できるようにする

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,19 @@ class Item < ApplicationRecord
   belongs_to :user
 
 
+  validates :image, presence: { message: "を選択してください" }
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :user, presence: true
+  validates :price, presence: true
+
+  with_options numericality: { other_than: 1, message: "を選択してください" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
+
 end
