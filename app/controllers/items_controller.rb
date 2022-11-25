b84948c662f126c26ad8c3ec75b89bad86baddf1
ponta4699 <ponta4699@gmail.com>
2022-11-25 13:31:18 +0900

    商品出品ページを表示できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,25 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  
+  def index
+    @items = Item.order(id: 'DESC')
+  end
 
 
   def new
     @item = Item.new
   end
 
+
+  
+
   private
 
-  def product_params
+  def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                     :scheduled_delivery_id, :image).merge(user_id: current_user.id)
   end
 
 
-
-  redirect_to action: :index
 
 end
