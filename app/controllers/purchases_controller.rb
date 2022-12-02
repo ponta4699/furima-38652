c9cf5bc15f647a77ffb8275ff307669431357f1c
ponta4699 <ponta4699@gmail.com>
2022-12-02 17:24:54 +0900

    フォームの情報を保存できるようにする

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :set_product, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  
   def index
     @purchase_address = PurchaseAddress.new
     return unless @item.user_id == current_user.id || !@item.purchase.nil?
@@ -9,6 +10,13 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+    
 
   end
 
@@ -16,7 +24,7 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_name,
-                                             :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+                                             :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
   end
 
   def item_params
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,15 +1,14 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
+  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
 
-  with_options presence: true do
+  with_options presence:true do
     validates :house_number
     validates :user_id
     validates :item_id
-    validates :token
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :post_code,format:{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :phone_number,format:{ with: /\A\d{10,11}\z/ }
   end
 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
